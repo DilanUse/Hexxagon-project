3f71fe198ded95240f47c8ed6026a0b3db7376a0
DilanUse <dilan_8-10@hotmail.com>
2016-10-30 16:00:05 -0400

    Actualización de la escena del juego
    Se termino de realizar la evalucacion de las adyacencias y coadyacencias
    en el tablero

--- Assets/Scripts/Juego.cs
+++ Assets/Scripts/Juego.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.UI;
-
+using System;
+using System.Threading;
 
 // espacio de nombres del Juego
 namespace Juego
@@ -27,13 +28,19 @@ namespace Juego
         Casilla[][] casillas; // casillas del tablero 
         List<Point> adyacentes; // Casillas adyacentes a una Casilla
         List<Point> coAdyacentes; // Casillas co-adyacentes a una Casilla
-
+        List<Point> auxiliarList; // lista para operaciones auxiliares 
+        Thread t;
 
         // se llama antes de la primera actualizacion grafica
         void Start()
         {
+
+            t = null;
+
+
             adyacentes = new List<Point>();
             coAdyacentes = new List<Point>();
+            auxiliarList = new List<Point>();
             int cantidadFilas = 5; // cantidad de filas a crear por cada columna
             int posicion = 0; // posicion de las Casillas
             TipoFicha tipoF = 0; // tipo de dicha de las Casillas
@@ -96,31 +103,78 @@ namespace Juego
         // Update is called once per frame
         void Update()
         {
+            if( this.t != null )
+            {
+                if( !t.IsAlive )
+                {
 
+                }
+            }
         }
 
 
         // se llama cuando se hace clic en una casilla
         public void Casilla_Click(int pos)
         {
-            this.adyacentes.Clear();
-            Point mypoint;
+            Thread t = new Thread(o =>
+            {
 
-            Debug.Log("Click" + pos);
+                foreach (var item in adyacentes)
+                {
+                    GameObject.Find("Borde" + casillas[item.x][item.y].Posicion).GetComponent<Image>().sprite =
+                        spritesJuego[(int)IndexSprite.VACIO];
+                }
 
-            mypoint = CoordenadasPorPosicion(pos);
+                foreach (var item in coAdyacentes)
+                {
+                    GameObject.Find("Borde" + casillas[item.x][item.y].Posicion).GetComponent<Image>().sprite =
+                        spritesJuego[(int)IndexSprite.VACIO];
+                }
 
-            Debug.Log(mypoint.x + "," + mypoint.y);
+
+
+                Point mypoint;
+                Debug.Log("Click" + pos);
+                mypoint = CoordenadasPorPosicion(pos);
+                Debug.Log(mypoint.x + "," + mypoint.y);
+
+
+                this.adyacentes.Clear();
+                BuscarAdyacentes(mypoint, adyacentes, null);
+                Debug.Log("Adyacentes: ");
+                foreach (var item in adyacentes)
+                {
+                    GameObject.Find("Borde" + casillas[item.x][item.y].Posicion).GetComponent<Image>().sprite =
+                        spritesJuego[(int)IndexSprite.BORDE_CLONACION];
+                    Debug.Log(casillas[item.x][item.y].Posicion);
+                }
+
+
+                this.coAdyacentes.Clear();
+                foreach (var item in adyacentes)
+                {
+                    auxiliarList.Clear();
+                    auxiliarList.AddRange(adyacentes);
+                    auxiliarList.Add(mypoint);
+                    auxiliarList.AddRange(coAdyacentes);
+
+                    BuscarAdyacentes(item, coAdyacentes, auxiliarList);
+                }
+
+
+                auxiliarList.Clear();
+                Debug.Log("Coadyacentes: ");
+                foreach (var item in coAdyacentes)
+                {
+                    GameObject.Find("Borde" + casillas[item.x][item.y].Posicion).GetComponent<Image>().sprite =
+                        spritesJuego[(int)IndexSprite.BORDE_SALTO];
+                    Debug.Log(casillas[item.x][item.y].Posicion);
+                }
+            });
+
+            t.Start();
 
             
-            BuscarAdyacentes(mypoint, adyacentes, null);
-
-            foreach (var item in adyacentes)
-            {
-                GameObject.Find("Borde" + casillas[item.x][item.y].Posicion).GetComponent<Image>().sprite =
-                    spritesJuego[(int)IndexSprite.BORDE_CLONACION];
-                Debug.Log(casillas[item.x][item.y].Posicion);
-            } 
 
         } // fin de Casilla_Click
 
@@ -141,67 +195,76 @@ namespace Juego
         // y las añade a la lista adyacentes sin incluir las Casillas de la lista excluidos
         private void BuscarAdyacentes( Point coordenadas, List<Point> adyacentes, List<Point> excluidos )
         {
-            coordenadas.y -= 1; // busco adyacente arriba
-            // si la fila es mayor a cero y no existen excluidos o la coordenada adyacente no esta excluida
-            if( coordenadas.y > 0 && (excluidos == null || (excluidos != null && excluidos.IndexOf( coordenadas) != -1 )) 
-                && casillas[coordenadas.x][coordenadas.y] != null )
-                adyacentes.Add(coordenadas);
-
-            coordenadas.y += 2; // busco adyacente abajo
-            // si la fila es menor a la longitud maxima y no existen excluidos o la coordenada adyacente no esta excluida
-            if (coordenadas.y < casillas[coordenadas.x].Length && casillas[coordenadas.x][coordenadas.y] != null && 
-                (excluidos == null || (excluidos != null && excluidos.IndexOf(coordenadas) != -1)))
-                adyacentes.Add(coordenadas);
-            coordenadas.y -= 1; // reestabllezo las coordenadas iniciales
-
-
-            // si la coordenada x es mayor a cero entonces existen adyacentes por la izquierda
-            if( coordenadas.x > 0 )
+            // atrapa excepciones al salirse de la representacion matricial del tablero
+            try
             {
-                coordenadas.x -= 1; // busco adyacentes por la izquierda
-                // si no existen excluidos o la coordenada adyacente no esta excluida
-                if ( casillas[coordenadas.x][coordenadas.y] != null && 
-                    (excluidos == null || (excluidos != null && excluidos.IndexOf(coordenadas) != -1)))
+                coordenadas.y -= 1; // busco adyacente arriba
+                // si la fila es mayor a cero y no existen excluidos o la coordenada adyacente no esta excluida
+                if (coordenadas.y >= 0 && (excluidos == null || (excluidos != null && excluidos.IndexOf(coordenadas) == -1))
+                    && casillas[coordenadas.x][coordenadas.y] != null)
                     adyacentes.Add(coordenadas);
 
-                // si la columna izquierda es menor, subo una fila, sino, bajo una fila
-                coordenadas.y += (coordenadas.x < 4 ? -1 : 1); // busco otro adyacente por la izquierda 
-                // si la fila es mayor a cero y menor al maximo tamaño
-                // y no existen excluidos o la coordenada adyacente no esta excluida
-                if (coordenadas.y > 0 && coordenadas.y < casillas[coordenadas.x].Length &&
-                    casillas[coordenadas.x][coordenadas.y] != null &&
-                    (excluidos == null || (excluidos != null && excluidos.IndexOf(coordenadas) != -1)))
+                coordenadas.y += 2; // busco adyacente abajo
+                // si la fila es menor a la longitud maxima y no existen excluidos o la coordenada adyacente no esta excluida
+                if (coordenadas.y < casillas[coordenadas.x].Length && casillas[coordenadas.x][coordenadas.y] != null &&
+                    (excluidos == null || (excluidos != null && excluidos.IndexOf(coordenadas) == -1)))
                     adyacentes.Add(coordenadas);
-
-                // reestablezco las coordenadas iniciales
-                coordenadas.y += (coordenadas.x < 4 ? 1 : -1);
-                coordenadas.x += 1;
-            } // fin del if
+                coordenadas.y -= 1; // reestabllezo las coordenadas iniciales
 
 
-            // si la coordenada x es menor al maximo tamaño posible entonces existen adyacentes por la derecha
-            if ( coordenadas.x < casillas.Length )
+                // si la coordenada x es mayor a cero entonces existen adyacentes por la izquierda
+                if (coordenadas.x > 0 )
+                {
+                    coordenadas.x -= 1; // busco adyacentes por la izquierda
+                    // si no existen excluidos o la coordenada adyacente no esta excluida
+                    if (coordenadas.y < casillas[coordenadas.x].Length && casillas[coordenadas.x][coordenadas.y] != null &&
+                        (excluidos == null || (excluidos != null && excluidos.IndexOf(coordenadas) == -1)))
+                        adyacentes.Add(coordenadas);
+
+                    // si la columna izquierda es menor, subo una fila, sino, bajo una fila
+                    coordenadas.y += (coordenadas.x < 4 ? -1 : 1); // busco otro adyacente por la izquierda 
+                    // si la fila es mayor a cero y menor al maximo tamaño
+                    // y no existen excluidos o la coordenada adyacente no esta excluida
+                    if (coordenadas.y >= 0 && coordenadas.y < casillas[coordenadas.x].Length &&
+                        casillas[coordenadas.x][coordenadas.y] != null &&
+                        (excluidos == null || (excluidos != null && excluidos.IndexOf(coordenadas) == -1)))
+                        adyacentes.Add(coordenadas);
+
+                    // reestablezco las coordenadas iniciales
+                    coordenadas.y += (coordenadas.x < 4 ? 1 : -1);
+                    coordenadas.x += 1;
+                } // fin del if
+
+
+                // si la coordenada x es menor al maximo tamaño posible entonces existen adyacentes por la derecha
+                if (coordenadas.x < casillas.Length -1)
+                {
+                    coordenadas.x += 1; // busco adyacentes por la derecha
+                    // si no existen excluidos o la coordenada adyacente no esta excluida
+                    if (coordenadas.y < casillas[coordenadas.x].Length && casillas[coordenadas.x][coordenadas.y] != null &&
+                        (excluidos == null || (excluidos != null && excluidos.IndexOf(coordenadas) == -1)))
+                        adyacentes.Add(coordenadas);
+
+                    // si la columna derecha es mayor, bajo una fila, sino, subo una fila
+                    coordenadas.y += (coordenadas.x > 4 ? -1 : +1); // busco otro adyacente por la derecha
+                    // si la fila es mayor a cero y menor al maximo tamaño
+                    // y no existen excluidos o la coordenada adyacente no esta excluida
+                    if (coordenadas.y >= 0 && coordenadas.y < casillas[coordenadas.x].Length &&
+                        casillas[coordenadas.x][coordenadas.y] != null &&
+                        (excluidos == null || (excluidos != null && excluidos.IndexOf(coordenadas) == -1)))
+                        adyacentes.Add(coordenadas);
+
+
+                    // reestablezco las coordenadas iniciales
+                    coordenadas.y += (coordenadas.x > 4 ? 1 : -1);
+                    coordenadas.x -= 1;
+                } // fin del if
+            }
+            catch(IndexOutOfRangeException e)
             {
-                coordenadas.x += 1; // busco adyacentes por la derecha
-                // si no existen excluidos o la coordenada adyacente no esta excluida
-                if ( casillas[coordenadas.x][coordenadas.y] != null &&
-                    (excluidos == null || (excluidos != null && excluidos.IndexOf(coordenadas) != -1)))
-                    adyacentes.Add(coordenadas);
-
-                // si la columna derecha es mayor, bajo una fila, sino, subo una fila
-                coordenadas.y += (coordenadas.x > 4 ? -1 : +1); // busco otro adyacente por la derecha
-                // si la fila es mayor a cero y menor al maximo tamaño
-                // y no existen excluidos o la coordenada adyacente no esta excluida
-                if (coordenadas.y > 0 && coordenadas.y < casillas[coordenadas.x].Length &&
-                    casillas[coordenadas.x][coordenadas.y] != null &&
-                    (excluidos == null || (excluidos != null && excluidos.IndexOf(coordenadas) != -1)))
-                    adyacentes.Add(coordenadas);
-
-                // reestablezco las coordenadas iniciales
-                coordenadas.y += (coordenadas.x > 4 ? 1 : -1);
-                coordenadas.x -= 1;
-            } 
-
+                Debug.Log(e.Message);
+                Debug.Log(coordenadas.x + ", " + coordenadas.y);
+            } // fin del try...catch
         } // fin de buscarAdyacentes
     } // fin de la clase Juego
 } // fin del espacio de nombres de Juego
